9a6d96b6324fd96f25c755ba155f0db4b586bb22
Zweihänder <zweidev@zweihander.me>
2023-06-14 20:49:13 -0400

    test(test): add in proper unit tests

--- test/org-statistics-cookie-helpers-test.el
+++ test/org-statistics-cookie-helpers-test.el
@@ -3,9 +3,10 @@
 ;; Copyright (C) 2021, Zweihänder <zweidev@zweihander.me>
 ;;
 ;; Author: Zweihänder
-;; Keywords: org-mode
+;; Keywords: outlines
 ;; Homepage: https://github.com/Zweihander-Main/org-statistics-cookie-helpers
 ;; Version: 0.0.1
+;; Package-Requires: ((emacs "27.1"))
 
 ;; This file is not part of GNU Emacs.
 
@@ -33,9 +34,49 @@
 (require 'buttercup)
 (require 'org-statistics-cookie-helpers)
 
-(describe "Testing"
-  (it "works."
-    (expect t :to-equal t)))
+(defvar org-statistics-cookie-helpers-test-temp-buffer nil)
+
+(describe "org-statistics-cookie-helpers"
+  (before-each
+    (setq org-statistics-cookie-helpers-test-temp-buffer
+          (generate-new-buffer "temp-buffer"))
+    (with-current-buffer org-statistics-cookie-helpers-test-temp-buffer
+      (org-mode)
+      (insert "* Heading 1")
+      (goto-char (point-min))
+      (org-statistics-cookie-helpers-insert-cookies)))
+
+  (after-each
+    (kill-buffer org-statistics-cookie-helpers-test-temp-buffer))
+
+  (it "should insert [%] cookie at the end of the line"
+    (with-current-buffer org-statistics-cookie-helpers-test-temp-buffer
+      (expect (buffer-substring-no-properties (point-min) (point-max))
+              :to-equal "* Heading 1 [100%]")))
+
+  (it "should find the statistics cookie on the line and return it as a plist"
+    (with-current-buffer org-statistics-cookie-helpers-test-temp-buffer
+      (let ((cookie (org-statistics-cookie-helpers-find-cookies)))
+        (expect cookie :to-equal
+                '(:begin 13 :end 19 :value "[100%]" :post-blank 0 :type %)))))
+
+  (it "should toggle between [/] and [%] type statistics cookies on the line"
+    (with-current-buffer org-statistics-cookie-helpers-test-temp-buffer
+      (org-statistics-cookie-helpers-toggle-cookies)
+      (expect (buffer-substring-no-properties (point-min) (point-max))
+              :to-equal "* Heading 1 [0/0]")
+      (org-statistics-cookie-helpers-toggle-cookies)
+      (expect (buffer-substring-no-properties (point-min) (point-max))
+              :to-equal "* Heading 1 [100%]")))
+
+  (it "should delete the statistics cookie on the line"
+    (with-current-buffer org-statistics-cookie-helpers-test-temp-buffer
+      (org-statistics-cookie-helpers-delete-cookies)
+      (expect (buffer-substring-no-properties (point-min) (point-max))
+              :to-equal "* Heading 1"))))
+
+
+(provide 'org-statistics-cookie-helpers-test)
 
 ;; Local Variables:
 ;; coding: utf-8
